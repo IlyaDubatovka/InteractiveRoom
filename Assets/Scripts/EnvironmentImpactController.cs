a6543e125ef0c155e0b79e8afb3c8c7cba3aa4d0
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-07-21 02:06:43 +0300

    feat: added lift and toss interactable items

--- Assets/Scripts/EnvironmentImpactController.cs
+++ Assets/Scripts/EnvironmentImpactController.cs
@@ -1,34 +1,53 @@
-
 using UnityEngine;
 
 public class EnvironmentImpactController : MonoBehaviour
 {
-    [SerializeField]private Camera _camera;
+    [SerializeField] private Camera _camera;
     [SerializeField] private Transform _inventoryHolder;
-    private Outline _outline;
+    private InteractableItem _interactableItem;
+    private InteractableItem _activeItem;
+
     private void Update()
     {
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
-        if (Physics.Raycast(ray,out hitInfo))
+        if (Physics.Raycast(ray, out hitInfo))
         {
-            if (hitInfo.transform.CompareTag(GlobalConstants.DOOR_TAG)&&Input.GetKeyDown(KeyCode.E))
+            if (hitInfo.transform.CompareTag(GlobalConstants.DOOR_TAG) && Input.GetKeyDown(KeyCode.E))
             {
                 var door = hitInfo.transform.GetComponent<Door>();
                 door.SwitchDoorState();
-                Debug.Log(hitInfo,gameObject);
             }
 
-            // if (hitInfo.transform.CompareTag(GlobalConstants.INTERECTABLE_OBJECT))
-            // {
-            //     Debug.Log(hitInfo,gameObject);
-            //     _outline = hitInfo.transform.GetComponent<Outline>();
-            //     _outline.OutlineWidth = 5;
-            // }
-            // else
-            // {
-            //     _outline.OutlineWidth = 0;
-            // }
+            if (hitInfo.transform.CompareTag(GlobalConstants.INTERECTABLE_OBJECT))
+            {
+                var currentTransform = hitInfo.transform;
+                _interactableItem = currentTransform.GetComponent<InteractableItem>();
+                _interactableItem.SetFocus();
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (_activeItem!=null)
+                    {
+                        _activeItem.ChangeLiftedStatus();
+                        _activeItem = null;
+                    }
+                    _activeItem = _interactableItem;
+                    _activeItem.ChangeLiftedStatus();
+                }
+            }
+
+            else if (_interactableItem != null)
+            {
+                _interactableItem.RemoveFocus();
+            }
+        }
+
+        if (_activeItem != null && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var rb = _activeItem.GetComponent<Rigidbody>();
+            _activeItem.ChangeLiftedStatus();
+            rb.AddForce(_camera.transform.forward * 20,ForceMode.Impulse);
+            _activeItem = null;
         }
     }
 }
--- Assets/Scripts/InteractableItem.cs
+++ Assets/Scripts/InteractableItem.cs
@@ -1,14 +1,19 @@
+using System;
 using UnityEngine;
 
 public class InteractableItem : MonoBehaviour
 {
     [SerializeField]
-    private int _highlightIntensity = 4;    
+    private int _highlightIntensity = 4;  
+    [SerializeField] private Transform _inventoryHolder;
     private Outline _outline;
+    private Collider _collider;
+    public bool isLifted { get; private set; } = false;
 
     private void Awake()
     {
         _outline = GetComponent<Outline>();
+        _collider = GetComponent<Collider>();
     }
 
     public void SetFocus()
@@ -19,5 +24,23 @@ public class InteractableItem : MonoBehaviour
     public void RemoveFocus()
     {
         _outline.OutlineWidth = 0;
+    }
+
+    public void ChangeLiftedStatus()
+    {
+        isLifted = !isLifted;
+    }
+
+    private void Update()
+    {
+        if (isLifted)
+        {
+            transform.position = _inventoryHolder.transform.position;
+            _collider.enabled = false;
+        }
+        else
+        {
+            _collider.enabled = true;
+        }
     }
 }
